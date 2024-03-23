d07b1ca6b97d52a44920287a6b20f02281d7eaa8
Usama Aslam <61847858+U7ama@users.noreply.github.com>
2024-03-23 14:05:23 +0500

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,18 +1,30 @@
+const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+const GEO_API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.REACT_APP_GEO_API_KEY,
+    'X-RapidAPI-Host': process.env.REACT_APP_GEO_API_HOST,
+  },
+};
+
 export async function fetchWeatherData(lat, lon) {
   try {
-    const response = await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/fetchWeatherData`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ lat, lon }),
-      }
-    );
-    const data = await response.json();
-    // console.log("fetchWeatherData", data.data);
-    return data.data;
+    let [weatherPromise, forcastPromise] = await Promise.all([
+      fetch(
+        `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+      fetch(
+        `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      ),
+    ]);
+
+    const weatherResponse = await weatherPromise.json();
+    const forcastResponse = await forcastPromise.json();
+    return [weatherResponse, forcastResponse];
   } catch (error) {
     console.log(error);
   }
@@ -21,15 +33,10 @@ export async function fetchWeatherData(lat, lon) {
 export async function fetchCities(input) {
   try {
     const response = await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}/fetchCities`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ input: input ? input : "" }),
-      }
+      `${GEO_API_URL}/cities?minPopulation=10000&namePrefix=${input}`,
+      GEO_API_OPTIONS
     );
+
     const data = await response.json();
     return data;
   } catch (error) {
@@ -37,3 +44,45 @@ export async function fetchCities(input) {
     return;
   }
 }
+
+
+
+// export async function fetchWeatherData(lat, lon) {
+//   try {
+//     const response = await fetch(
+//       `${process.env.REACT_APP_BACKEND_URL}/fetchWeatherData`,
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ lat, lon }),
+//       }
+//     );
+//     const data = await response.json();
+//     // console.log("fetchWeatherData", data.data);
+//     return data.data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// export async function fetchCities(input) {
+//   try {
+//     const response = await fetch(
+//       `${process.env.REACT_APP_BACKEND_URL}/fetchCities`,
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({ input: input ? input : "" }),
+//       }
+//     );
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//     return;
+//   }
+// }
